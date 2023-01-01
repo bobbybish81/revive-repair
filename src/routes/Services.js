ac12972e1051131ddeb49f83c0e2a1112331e306
bobbybish81 <bobbybish81@hotmail.com>
2023-01-01 19:29:17 +0100

    services text changed

--- src/routes/Services.js
+++ src/routes/Services.js
@@ -36,11 +36,11 @@ const Services = () => {
         <article className="services">
           <h2>OUR SERVICE</h2>
           <p className="services-text">We are a fast, professional and fully accredited work safe contractor who save our customers time and money by <b style={{fontWeight: '700'}}>NOT</b> replacing but repairing damaged items on site.</p>
-          <p className="services-text">Scratches, dents, cracks, stains and scuff marks or even just fatigued surfaces can be revived to look as good as new by our fully trained technicians.</p>
+          <p className="services-text">Scratches, dents, cracks, stains, scuff marks or even just fatigued surfaces can be revived to look as good as new by our fully trained technicians.</p>
         </article>
         <article className="services">
           <h2>OUR COVERAGE</h2>
-          <p className="services-text">Whether you require a small one off small repair or you need us to visit a number of sites, Revive can help.</p>
+          <p className="services-text">Whether you require a small one off repair or you need us to visit a number of sites, Revive can help.</p>
           <p className="services-text">We cover a large area across the South East including:</p>
           <div className="area-list-container">
             <ul className="area-list">
@@ -64,7 +64,7 @@ const Services = () => {
         <article className="services">
           <h2>OUR OFFER</h2>
           <p className="services-text">Revive Ltd have the skills and experience to restore, repair and renew virtually any hard surface both internally and externally with over 16 years’ experience within this industry.</p>
-          <p className="services-text">We pride ourselves on the quality of our work. We are confident that we can offer very competitive rates compared to other larger competitors with more overheads where quantity may compromise on quality!</p>
+          <p className="services-text">We pride ourselves on the quality of our work. We are confident that we can offer very competitive rates compared to much larger competitors with higher overheads where quantity may compromise on quality!</p>
         </article>
       </section>
       <h2 className="services-detail-heading">WE CAN REVIVE ALL THIS AND MORE</h2>
